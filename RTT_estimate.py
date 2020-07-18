1da2ac53b77f13004c2abff87ebf1aa4c2c9ed0f
TTTriplicate <tttriplicate@gmail.com>
2020-07-18 08:21:03 -0400

    explanation and idea for further use

--- RTT_estimate.py
+++ RTT_estimate.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+'''
+Written for a networking class.  With a couple of givens, calculate
+the estimated RTT and timeout interval i accordance with the guide-
+lines laid out in RFC 6298.
 
+Could be quickly modified to take any values or samples,
+or manually altered to different givens.
+'''
 def updateEst(currEst,currDev, sample):
     alpha = .1
     beta = .25
